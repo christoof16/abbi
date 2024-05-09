1d32d37c1631fe26884e4400099156f36389c83b
chris <christoof16@gmail.com>
2024-05-08 19:28:41 -0600

    simulador de ventas de cursos

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+
+window.swiper = new Swiper({
+    el: '.populares__cursos',
+    slideClass: 'populares__curso',
+    createElements: true,
+    autoplay:{
+        delay: 5000
+    },
+    loop:true,
+    navigation: true,
+    pagination: true
+});
